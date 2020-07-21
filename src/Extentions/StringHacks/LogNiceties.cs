2427470e0cb7f9ba8dacd76f04f6785717c3c64c
Aidan <aidan@pop-os.localdomain>
2020-07-21 22:14:30 +0100

    Formatting

--- src/Extentions/StringHacks/LogNiceties.cs
+++ src/Extentions/StringHacks/LogNiceties.cs
@@ -2,37 +2,37 @@ namespace TextureJinn.Extentions.StringHacks
 {
     public static class LogNiceties
     {
-        private const string StandardSeperator = "====================";
-        private const string LineEnding = "\n";
+        private const string m_StandardSeperator = "====================";
+        private const string m_LineEnding = "\n";
 
         /// <summary> Appens a standardised seperator </summary>
         public static string AppendSeperator(this string str) {
-            return string.Concat(str, StandardSeperator);
+            return string.Concat(str, m_StandardSeperator);
         }
 
         /// <summary> Appens a line ending then a sepperator </summary>
         public static string AppendLineSeperator(this string str) {
-            return string.Concat(str, LineEnding, StandardSeperator);
+            return string.Concat(str, m_LineEnding, m_StandardSeperator);
         }
 
         /// <summary> Appens a line ending, a sepperator then one more line ending to the end of that </summary>
         public static string AppendPaddedSep(this string str) {
-            return string.Concat(str, LineEnding, StandardSeperator, LineEnding);
+            return string.Concat(str, m_LineEnding, m_StandardSeperator, m_LineEnding);
         }
 
         /// <summary> Appens two line endings, a sepperator then two line endings to the end of that </summary>
         public static string Append2PadSep(this string str) {
-            return string.Concat(str, LineEnding, LineEnding, StandardSeperator, LineEnding, LineEnding);
+            return string.Concat(str, m_LineEnding, m_LineEnding, m_StandardSeperator, m_LineEnding, m_LineEnding);
         }
 
         /// <summary> Appens a sepperator then a single line ending to the end of that </summary>
         public static string AppendPstPadSep(this string str) {
-            return string.Concat(str, StandardSeperator, LineEnding);
+            return string.Concat(str, m_StandardSeperator, m_LineEnding);
         }
 
         /// <summary> Appens a sepperator then two line endings to the end of that </summary>
         public static string AppendPst2PadSep(this string str) {
-            return string.Concat(str, StandardSeperator, LineEnding, LineEnding);
+            return string.Concat(str, m_StandardSeperator, m_LineEnding, m_LineEnding);
         }
     }
 }
