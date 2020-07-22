6a568ec2fb46455a2d002f74d935d6b8a6c1ef7b
Aidan <aidan@pop-os.localdomain>
2020-07-22 22:44:46 +0100

    Re-jigged program.cs to go back to a windowing focus

--- src/Main/Program.cs
+++ src/Main/Program.cs
@@ -5,6 +5,7 @@ using SkiaSharp;
 
 using TextureJinn.Config;
 
+using TextureJinn.Rendering.Windowing;
 using TextureJinn.Rendering.Rasterisation;
 using TextureJinn.Rendering.Rasterisation.SVG;
 
@@ -18,31 +19,10 @@ namespace TextureJinn
     {
         static void Main(string[] args)
         {
-            // Console.WriteLine("Hello World!\n".AppendPst2PadSep() + "Loading window...");
-
-            // Root root = new Root();
-
-            // Console.WriteLine("Window closed".Append2PadSep());
-
             TypefaceManager.s_TypefaceQue.Enqueue("Quantico");
             TypefaceManager.s_Install();
-
-            SvgRenderer svg = TJCfg.Svgs.GetSvg("SplashBase.svg");
-
-            svg.SaveBitmap(svg.Render(new Vector2Di(-1, 2048)), "test.png");
-
-            string data = File.ReadAllText("SplashBase.svg".PrependVectorDir());
             
-            data = data.Replace("tj_text_s:2", TJCfg.TJVersion.VerNum);
-            data = data.Replace("tj_text_s:1", TJCfg.FunFact);
-
-            for (int i = 0; i < 850; i++)
-            {
-                Console.WriteLine(TJCfg.FunFact);
-            }
-
-            svg = new SvgRenderer(data, false);
-            svg.Render(new Vector2Di(-1, 2048), "test2.png");
+            Root root = new Root();
         }
     }
 }
